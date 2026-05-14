b5c13923f6ddd7e991193e94b314e4cbb35ae61f
朱松松 <zss@email.com>
2026-05-14 15:54:18 +0800

    初始化项目

---
+++ src/main/java/com/demo/cloud/embedding/EmbeddingApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cloud.embedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddingApplication.class, args);
+    }
+
+}
